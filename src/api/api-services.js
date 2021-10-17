789ff75491333498a98c7360a5c6570b9e4ad645
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-17 21:10:59 +0300

    edit api

--- src/api/api-services.js
+++ src/api/api-services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const key = "23131793-6d85f6b0cc60a2b86f77a0160";
 
-axios.defaults.baseUrl = "https://pixabay.com/api";
+axios.defaults.baseURL = "https://pixabay.com/api";
 
 const fetchImages = async (query, currentPage) => {
   const { data } = await axios.get(
